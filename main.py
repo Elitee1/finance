00369331ce2661baf43cdf4e16e923c2485a8b6e
Suhail <suhail@MacBookPro.fritz.box>
2025-06-29 14:04:48 +0200

    customtkinter to streamlit

--- main.py
+++ main.py
@@ -1,42 +1,29 @@
-import customtkinter
-import pandas as pd
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
 
 
-def main():
-    datei = pd.read_csv("files/ausgaben.csv")
+st.set_page_config(page_title="Finance Manager", page_icon="💰", layout="wide")
 
-    
-    app = customtkinter.CTk()
-    app.geometry("800x600")
-    app._set_appearance_mode("System")
-    app.title("Finance")
-
-    # Haupt-Frames
-    kategorieFrame = customtkinter.CTkFrame(app, width=200)
-    kategorieFrame.pack(side="left", fill="y", pady=12, padx=12)
-
-    contentFrame = customtkinter.CTkFrame(app)
-    contentFrame.pack(side="right", expand=True, fill="both", pady=12, padx=12)
-    contentFrame.pack_propagate(False)
-
-    def frameSpawn(betrag):
-        for widget in contentFrame.winfo_children():
-            widget.destroy()
-        label = customtkinter.CTkLabel(contentFrame, text=f"{betrag} €")
-        label.pack(pady=20)
-        print(f"Angezeigt: {betrag} €")
-
-    
-    for kategorie, betrag in zip(datei["Kategorie"], datei["Betrag"]):
-        button = customtkinter.CTkButton(
-            kategorieFrame,
-            text=f"{kategorie}",
-            command=lambda b=betrag: frameSpawn(b)
-        )
-        button.pack(pady=6, padx=12, fill="x")
-
-    app.mainloop()
+def load_transaction(file):
+    try: 
+        df = pd.read_csv(file)
+        df.columns = [col.strip() for col in df.columns]
+        
+        st.write(df)
+        return df 
+    except Exception as e: 
+        st.error(f"error: {str(e)}")
+        return None 
 
 
-if __name__ == "__main__":
-    main()
+def main(): 
+    st.title("Finance Manager")
+
+    uploadFile = st.file_uploader("Upload your transaction CSV file", type=["csv"])
+
+    if uploadFile is not None: 
+        df = load_transaction(uploadFile)
+
+main()
+
